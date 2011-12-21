09cc0223fa832c4245dcc5f3edfcf98a8079e7f2
ErikNeurohr <ErikNeurohr@gmail.com>
2011-12-21 14:17:19 +0000

    parser aus allen codeständen außer erzeugtem parser

--- NASE-Erik/src/compiler/parsers/ParserFactory.java
+++ NASE-Erik/src/compiler/parsers/ParserFactory.java
@@ -9,7 +9,9 @@ public class ParserFactory {
 
 	public static ParserWrapper getParser(String[] args, ScannerWrapper scanner){
 		List<String> arguments = Arrays.asList(args);
-		if(arguments.contains("-blxif"))
+		if(arguments.contains("-blx"))
+			return new ParserBLX(scanner);
+		else if(arguments.contains("-blxif"))
 			return new ParserBLXIF(scanner);
 		else
 			return new Parser(scanner);
